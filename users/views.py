dd50310965fde2ccda378060edb0711bf1a15337
Max <lostambel@gmail.com>
2024-05-22 16:02:42 +0300

    Implement RegistrationView

--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import UserModel
 from .serializers import UserSerializer
 
 
 class RegisterView(APIView):
+    serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
+
     @staticmethod
     def post(request):
         serializer = UserSerializer(data=request.data)
