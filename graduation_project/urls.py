88c5eb2dd81fa0e22c44687037b1e49fd54e0e98
Max <lostambel@gmail.com>
2024-05-23 12:53:15 +0300

    Init platform app urls file

--- graduation_project/urls.py
+++ graduation_project/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
 
     path('api/', include('users.urls')),
 
+    path('api/', include('platform_app.urls')),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
