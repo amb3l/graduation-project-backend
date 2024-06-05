bc832d2f54070d40f381768d7496c33f88489f4b
Max <lostambel@gmail.com>
2024-06-05 17:53:54 +0300

    Implement OrderModel

--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,29 @@
+from datetime import datetime
+
 from django.db import models
 
 
 class OrderModel(models.Model):
-    number = models.CharField(max_length=20)
-
     sender = models.ForeignKey('UserModel', on_delete=models.CASCADE)
     receiver = models.ForeignKey('UserModel', on_delete=models.CASCADE)
 
     sender_platform = models.ForeignKey('PlatformModel', on_delete=models.CASCADE)
     receiver_platform = models.ForeignKey('PlatformModel', on_delete=models.CASCADE)
 
+    delivery_cost = models.FloatField()
+    payment_method = models.CharField(max_length=25)
+
+    STATUSES = models.Choices(
+        ('ACTIVE', 'Активный'),
+        ('CANCELED', 'Отменён'),
+        ('RECEIVED', 'Доставлен'),
+    )
+
+    status = models.CharField(max_length=10, choices=STATUSES)
+
+    date_created = models.DateTimeField(default=datetime.now())
+    date_canceled = models.DateTimeField()
+    date_received = models.DateTimeField()
+
     def __str__(self):
-        return f'{self.number}'
+        return f'{self.pk}'
