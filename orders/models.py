8de7bafa469996102279e70c38593d5c11d814e9
Max <lostambel@gmail.com>
2024-06-05 16:34:03 +0300

    Init Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class OrderModel(models.Model):
+    number = models.CharField(max_length=20)
+
+    sender = models.ForeignKey('UserModel', on_delete=models.CASCADE)
+    receiver = models.ForeignKey('UserModel', on_delete=models.CASCADE)
+
+    sender_platform = models.ForeignKey('PlatformModel', on_delete=models.CASCADE)
+    receiver_platform = models.ForeignKey('PlatformModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.number}'
